c39365f73a558a323b9bb7dcb4b5b112de4331f4
david <david17895@gmail.com>
2020-12-20 14:06:38 +0200

    dishes ammount logic

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('תמונות בלבד');
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post('/', upload.single('file'), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const recipeRoutes = require('./routes/recipeRoutes');
-
+const uploadRoutes = require('./routes/uploadRoutes');
 dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use('/api/recipes', recipeRoutes);
+app.use('/api/upload', uploadRoutes);
+
+app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 5001;
 
